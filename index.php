5cdb1df7340fa269bcac39a16353e7f29662afb6
dichor94 <dichor@alumnatflorida.es>
2024-12-15 15:25:16 +0100

    Añado el código final y la captura de pantalla dada en el ejercicio

---
+++ index.php
@@ -0,0 +1,402 @@
+<?php
+$pomodoroHaters = [
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '0', '0', 'A', '0', 'A', '0', '0', 'A', '0', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '0', '0', '0', '0', '0', '0', 'A', '0', '0', '0', '0', 'A', '0', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '0', '0', '0', '0', '0', 'A', 'A', '0', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '0', '0', '0', '0', '0', 'A', '0', '0', 'A', 'A', 'A', '0', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '0', '0', '0', 'A', '0', 'A', 'A', '0', '0', '0', '0', '0', '0', '0', 'A', '0', '0', '0', 'A', '0', '0', '~'],
+    ['~', '~', '~', '~', '~', '~', '0', '0', 'A', '0', '0', '0', 'A', '0', '0', '0', '0', '0', '0', 'A', '0', '0', '0', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '~'],
+    ['~', '~', '~', '~', '~', '0', '0', 'A', '0', '0', '0', '0', '0', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '0', '0', '0', '0', '0', 'A', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '~', '~', '~'],
+    ['~', '~', '~', '0', '0', '0', '0', '0', '0', '0', 'A', '0', '0', '0', '0', '0', '0', '0', '0', '~', '~', '~', '~', '~'],
+    ['~', '0', '0', '0', '0', '0', '0', '0', '0', 'A', '0', '0', '0', 'A', '0', '0', '0', '0', '0', 'A', '0', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '0', '0', '0', '0', 'A', '0', '0', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '0', '0', '0', 'A', 'A', 'A', '0', '0', 'A', '0', '0', '0', '~', '~', '~'],
+    ['~', '~', '~', '~', '0', 'A', 'A', '0', '0', 'A', '0', '0', '0', 'A', '0', '0', '0', '0', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '0', 'A', '0', '0', '0', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '0', '0', '0', 'A', '0', '0', '0', '0', '0', 'A', '0', 'A', '0', '0', '0', '~'],
+    ['~', '~', '~', '0', 'A', '0', '0', '0', '0', '0', '0', '0', '0', 'A', '0', '0', '0', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '0', '0', '0', 'A', '0', '0', 'A', '0', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '0', '0', '0', '0', 'A', '0', '0', '0', '0', 'A', '0', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'A', '0', '0', '0', '0', '0', '0', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '0', '0', '0', '0', '0', 'A', '0', '0', '0', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~']
+];
+
+
+$impacts = [
+  [20, 4],
+  [2, 13],
+  [13, 12],
+  [3, 17],
+  [2, 13],
+  [5, 19],
+  [6, 18],
+  [5, 2],
+  [20, 13],
+  [9, 7],
+  [5, 9],
+  [15, 16],
+  [16, 13],
+  [16, 9],
+  [16, 0],
+  [3, 19],
+  [19, 8],
+  [1, 16],
+  [18, 4],
+  [21, 23],
+  [7, 17],
+  [22, 15],
+  [21, 6],
+  [14, 8],
+  [12, 23],
+  [7, 7],
+  [22, 4],
+  [3, 21],
+  [2, 3],
+  [8, 11],
+  [0, 4],
+  [7, 21],
+  [11, 4],
+  [7, 15],
+  [6, 17],
+  [5, 19],
+  [4, 19],
+  [4, 7],
+  [23, 21],
+  [15, 20],
+  [2, 9],
+  [21, 2],
+  [1, 13],
+  [7, 10],
+  [21, 5],
+  [13, 17],
+  [2, 14],
+  [11, 14],
+  [22, 17],
+  [18, 22],
+  [2, 23],
+  [3, 1],
+  [18, 6],
+  [17, 12],
+  [18, 18],
+  [20, 2],
+  [3, 14],
+  [11, 21],
+  [6, 5],
+  [6, 2],
+  [12, 23],
+  [18, 18],
+  [21, 6],
+  [10, 12],
+  [5, 4],
+  [16, 19],
+  [8, 10],
+  [12, 21],
+  [15, 1],
+  [20, 14],
+  [3, 20],
+  [6, 19],
+  [20, 13],
+  [15, 4],
+  [10, 2],
+  [5, 16],
+  [20, 1],
+  [12, 13],
+  [11, 5],
+  [12, 14],
+  [8, 3],
+  [6, 8],
+  [19, 7],
+  [16, 9],
+  [13, 20],
+  [3, 5],
+  [1, 0],
+  [20, 14],
+  [12, 21],
+  [12, 16],
+  [15, 7],
+  [9, 1],
+  [1, 18],
+  [20, 6],
+  [8, 6],
+  [22, 1],
+  [10, 22],
+  [19, 19],
+  [7, 16],
+  [8, 8]
+];
+
+/*Mostrar el resultado de las funciones y otras operaciones*/
+
+/*Punto 1 - mostrar el contenido del mapa original mediante la función showMap*/
+
+echo "MAPA ORIGINAL" . "<br>";
+echo showMap($pomodoroHaters) . "<br><br>";
+
+
+/*Punto 2*/
+$mapC = impactsC($pomodoroHaters, $impacts);
+
+echo "MAPA CON IMPACTOS URBANOS" . "<br>";
+echo showMap($mapC);
+
+echo "<br><br>";
+
+/*Punto 3*/
+/*Hago una variable para calcular el total de LITROS usando la función que hemos creado para este punto "totalPersons, también lo usaremos para hacer las frases correspondientes. Se pide en este punto que la función retorne un valor entero"*/
+
+/*Para asegurarnos usamos la función de intval para obtener un número entero*/
+
+$totalColirioL = intval((totalPersons($mapC) * 25) / 1000);
+echo "HAY UN TOTAL DE " ."<strong>".totalPersons($mapC) ."</strong>" ." PERSONAS AFECTADAS." . "<br>" . "SE NECESITA UN TOTAL DE DE <strong>$totalColirioL</strong> L DE COLIRIO";
+echo "<br><br>";
+
+/*Punto 4 Impactos en zonas no habitadas y agua ()*/
+
+echo "MAPA CON IMPACTOS EN ZONAS HABITADAS, ZONAS NO HABITADAS Y MAR <br>";
+
+//Uno esta variable declarada anteriormente $impactC = impactsC($pomodoroHaters, $impacts);
+$mapSxc = impactSX($mapC, $impacts);
+
+echo showMap($mapSxc) . "<br><br>";
+
+
+  
+/*Punto 5 Calcular impactos totales de zonas afectadas hurbanas y no hurbanas*/
+
+$viewCostTotal = costDamage($mapSxc); 
+echo "HA HABIDO UNOS DAÑOS TOTALES DE: " .$viewCostTotal. "€"  ."<br><br>";
+
+
+
+
+/*Punto 6 calcular el total de agua y el total de agua afectada, cada uno en una función distinta totalWater y totalDamageWater, también del total de bakalao que disponen sabiendo que hay 2000T de bakalao*/
+
+$totalWater = totalWater($pomodoroHaters); //Creo variable para usarlo para indicar la función
+$totalDamageWater = totalDamageWater($mapSxc);
+$totalBakalao = intval((2000/$totalWater)*$totalDamageWater);
+
+
+echo "HAY $totalDamageWater km^2 AFECTADOS DE UN TOTAL DE $totalWater km^2 DE MAR <br>"; //Mostramos el resultado final con los dos resultados de las funciones
+echo "HAY UN TOTAL DE $totalBakalao TONELADAS DE PESCADO <br>";
+
+
+
+/*Punto 7 cuanto recauda la ONG si cada KG de bakalao se puede vender por 5€ (elaborado)*/
+
+
+$totalOng = $totalBakalao * 5;
+echo "PODEMOS GANAR: $totalOng € VENDIENDO PESCADO";
+
+
+
+
+
+/*FUNCIONES REALIZADAS*/
+
+
+/*Ejercicio 1*/
+function showMap(array $map): string{ //Indico que la entrada es un tipo array y que debe retornar un string (en este caso)
+
+  $mapVar = "";
+  foreach ($map as $pomoKey => $pomoValue){
+
+      foreach($pomoValue as $values){
+
+          $mapVar .= $values;
+
+
+
+      }
+      $mapVar .= "<br>";
+  }
+
+  return $mapVar;
+
+}
+
+
+/*Ejercicio 2 -Función que pasamos por parámetros el mapa original y los impactos, retornará un nuevo array (posteriormente con un bucle mostraremos el contenido del array). En esta función cambiamos la A por la C (zonaz impactadas)*/
+function impactsC(array $map, array $impact): array{ //Indco que la entrada es tipo array y la salida es del mismo tipo en este caso (se pide que retorne un nuevo array)
+
+  $pomodoroHaters2 = [];
+
+  foreach($map as $pomoKey => $pomoValue){
+
+      $pomodoroHaters2[] = array_slice($pomoValue, 0);
+  
+  }
+  
+  $numI = 0;
+  $numJ = 0;
+
+  for($i = 0; $i < count($impact); $i++){
+      for($j=0; $j<count($impact[$i]); $j++){
+
+          $numI = $impact[$i][0];
+          $numJ = $impact[$i][1];
+
+          if($pomodoroHaters2[$numI][$numJ] == "A"){
+
+              $pomodoroHaters2[$numI][$numJ] = "C";
+
+          }
+      }
+  }   
+
+  return $pomodoroHaters2;
+
+}
+
+/*Ejercicio 3 Función que retorna un valor que es el total de personas*/
+
+function totalPersons(array $map): int{
+
+  $totalPerson = 0; //Variable para añadir el número de personas
+  $totalColirio = 0;
+  
+
+  foreach($map as $keyMap => $valueMap){
+
+      foreach($valueMap as $value){
+
+          if($value == "C"){
+
+              $totalPerson += 5000;
+
+
+          }
+
+      }
+  }
+  return $totalPerson;
+}
+
+
+
+/*Ejercicio 4 - Función para mostrar el mapa con los nuevos impactos en zonas no habitadas y en el mar, se pasarán dos valores siendo en mapa (usaré la función de impactC) y el array de los impactos*/
+
+
+function impactSX(array $map,array $impact): array{
+
+  $pomodoroHaters2 = [];
+
+  foreach($map as $mapKey => $mapValue){
+
+    $pomodoroHaters2[] = array_slice($mapValue, 0);
+
+  }
+
+  foreach($impact as $keyImpact => $valueImpact){
+
+    foreach($pomodoroHaters2 as $keyMap => &$valueMap){ //Si no pongo la referencia a $valueMap no cambiará el valo del mismo, si pongo la referencia se cambiará en esa "celda" en específico
+
+    //La referencia se puede poner para una variable de fuera de la función como una variable de dentro de la función.
+
+        if($keyMap == $valueImpact[0] && $valueMap[$valueImpact[1]] == "0"){
+
+            $valueMap[$valueImpact[1]] = "X";
+        
+        }else if($keyMap == $valueImpact[0] && $valueMap[$valueImpact[1]] == "~"){
+
+            $valueMap[$valueImpact[1]] = "S";
+
+        }
+
+
+    }
+
+}
+  return $pomodoroHaters2;
+}
+
+
+
+/*Ejercicio 5: calcular daños siendo ZONA NO HURBANA (50.000 km  cuadrado - X) y en ZONA HURBANA (200.000 km cuadrado - C)*/
+
+
+
+function costDamage(array $map) : int{
+
+    $costNoHurban = 0;
+    $costHurban = 0;
+
+  foreach ($map as $keyMap => $valueMap){
+
+    foreach ($valueMap as $value){
+
+      if($value === "C"){
+
+        $costHurban += 200000;
+
+      }else if( $value === "X"){
+
+        $costNoHurban += 50000;
+
+      }
+
+    }
+
+
+  }
+
+  $total = $costNoHurban + $costHurban;
+
+  return $total;
+
+}
+
+/*Ejercicio 6: Función para contar el total de agua que hay y otra función para retornar solamente el mar afectado*/
+
+function totalWater(array $map) : int{
+
+  $totalWater = 0;
+
+  foreach($map as $keyMap => $valueMap){
+
+    foreach($valueMap as $value){
+
+      if($value == "~"){
+
+        $totalWater += 1;
+
+
+      }
+
+    }
+
+  }
+
+  return $totalWater;
+
+}
+
+
+function totalDamageWater(array $map): int{
+
+  $totalWater = 0;
+
+  foreach($map as $keyMap => $valueMap){
+
+    foreach($valueMap as $value){
+
+      if($value == "S"){
+
+        $totalWater += 1;
+
+
+      }
+
+    }
+
+  }
+
+  return $totalWater;
+
+}
+
+?>
